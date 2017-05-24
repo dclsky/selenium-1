aaeaef7579fc937c1ce3cbf7e2da95a77ed8f54f
wycaoxin <wycaoxin@qq.com>
2017-05-24 19:08:46 +0800

    添加了注释说明

--- WebDriver API/mouse_events/right_click.py
+++ WebDriver API/mouse_events/right_click.py
@@ -1,12 +1,12 @@
+# 模拟鼠标右击操作
 from selenium import webdriver
 # 引入ActionChains类
 from selenium.webdriver.common.action_chains import ActionChains
-import time
 
 driver = webdriver.Chrome()
 driver.get("http://pan.baidu.com/")
 
-driver.find_element_by_link_text("帐号密码登录").click()
-
-right_click = driver.find_element_by_id("s_lg_img")
+# 定位到要右击的元素
+right_click = driver.find_element_by_id("XXX")
+# 对定位到的元素执行鼠标右击操作
 ActionChains(driver).context_click(right_click).perform()
