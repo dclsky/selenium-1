b50d068e73d1461d51f887067a54d89495726db7
wycaoxin <wycaoxin@qq.com>
2017-05-24 21:39:06 +0800

    添加了鼠标双击代码

---
+++ WebDriver API/mouse_events/double_click.py
@@ -0,0 +1,11 @@
+# 模拟鼠标双击操作
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Chrome()
+driver.get("XXX")
+
+# 定位到要双击的元素
+double_click = driver.find_element_by_id("XXX")
+# 对定位到的元素执行双击操作
+ActionChains(driver).double_click(double_click).perform()
