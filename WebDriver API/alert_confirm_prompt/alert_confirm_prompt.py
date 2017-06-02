a20a17eef90d4445cc59084f0851234e27549875
wycaoxin <wycaoxin@qq.com>
2017-06-02 19:01:14 +0800

    添加了prompt弹窗的处理方法

--- WebDriver API/alert_confirm_prompt/alert_confirm_prompt.py
+++ WebDriver API/alert_confirm_prompt/alert_confirm_prompt.py
@@ -5,21 +5,48 @@ driver = webdriver.Chrome()
 file_path = 'file:///' + os.path.abspath("alert_confirm_prompt.html")
 driver.get(file_path)
 
+# 点击alert弹窗并确定
 driver.find_element_by_xpath("//*[@id='btnAlert']").click()
 time.sleep(3)
-print(driver.switch_to.alert.text)
-driver.switch_to.alert.accept()
+alert1 = driver.switch_to.alert
+print(alert1.text)
+alert1.accept()
 time.sleep(3)
 
+# 点击confirm弹窗并确定
 driver.find_element_by_xpath("//*[@id='btnConfirm']").click()
 time.sleep(3)
-print(driver.switch_to.alert.text)
-driver.switch_to.alert.accept()
+confirm1 = driver.switch_to.alert
+print(confirm1.text)
+confirm1.accept()
 time.sleep(3)
 
+# 点击confirm弹窗并取消
 driver.find_element_by_xpath("//*[@id='btnConfirm']").click()
 time.sleep(3)
-print(driver.switch_to.alert.text)
-driver.switch_to.alert.dismiss()
+confirm2 = driver.switch_to.alert
+print(confirm2.text)
+confirm2.dismiss()
 time.sleep(3)
 
+# 点击prompt弹窗并输入"abc",然后点击确定
+driver.find_element_by_xpath("//*[@id='btnPrompt']").click()
+time.sleep(3)
+prompt1 = driver.switch_to.alert
+print(prompt1.text)
+prompt1.send_keys('abc')
+time.sleep(3)
+prompt1.accept()
+time.sleep(3)
+
+# 点击prompt弹窗并输入"cba",然后点击取消
+driver.find_element_by_xpath("//*[@id='btnPrompt']").click()
+time.sleep(3)
+prompt2 = driver.switch_to.alert
+print(prompt2.text)
+prompt2.send_keys('cba')
+time.sleep(3)
+prompt2.dismiss()
+time.sleep(3)
+
+driver.quit()
